d44c10ab6f166dfec838af01f09f3fbfd7be6cb4
MOMOSTAFA0 <93434357+MOMOSTAFA0@users.noreply.github.com>
2024-10-19 23:31:43 +0300

    Update IStudentRepo.cs

--- ELearningPlatform/Repositery/IStudentRepo.cs
+++ ELearningPlatform/Repositery/IStudentRepo.cs
@@ -4,7 +4,7 @@ namespace DEPI_Project.Repositories
 {
     public interface IStudentRepo
     {
-        public Task Add_Student(Student Course, ApplicationUser StudentAccount);
+        public async Task<IdentityResult> Add_Student(Student Course, ApplicationUser StudentAccount);
         public void Update_Student(int id, Student student, ApplicationUser StudentAccount);
         public Task Delete_Student(Student student, ApplicationUser StudentAccount);
         public Student Get_StudentByID(int id);
